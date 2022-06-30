1dd54af6f0a62729f0e03e20a4ec512a64b2a859
asjay18 <sejin1314@korea.ac.kr>
2022-06-30 16:00:05 +0900

    item 속성 정의

--- app/src/main/java/com/example/cs496_1stweek/contact/ContactItem.kt
+++ app/src/main/java/com/example/cs496_1stweek/contact/ContactItem.kt
@@ -1,19 +1,20 @@
 package com.example.cs496_1stweek.contact
 
+import android.media.Image
 import com.example.cs496_1stweek.R
 
 class ContactItem {
     fun loadRecycleview(): List<RecycleView> {
         return listOf<RecycleView>(
-            RecycleView(R.string.recycleview1),
-            RecycleView(R.string.recycleview2),
-            RecycleView(R.string.recycleview3),
-            RecycleView(R.string.recycleview4),
-            RecycleView(R.string.recycleview5),
-            RecycleView(R.string.recycleview6)
+            RecycleView(0,"aaa","111"),
+            RecycleView(0,"bbb","222"),
+            RecycleView(0,"ccc","333"),
+            RecycleView(0,"ddd","444"),
+            RecycleView(0,"eee","555"),
+            RecycleView(0,"fff","666")
         )
     }
 
 }
 
-data class RecycleView(val stringResourceID: Int)
+data class RecycleView (var pic: Int, var name: String, var phoneNum: String)
