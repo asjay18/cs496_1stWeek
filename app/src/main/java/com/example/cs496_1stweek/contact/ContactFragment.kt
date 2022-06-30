0dc5f1bf78425b37db2a37519b4ae9757138e61c
star <vm@star.local>
2022-06-30 23:06:15 +0900

    loadcontact() 함수로 합침

--- app/src/main/java/com/example/cs496_1stweek/contact/ContactFragment.kt
+++ app/src/main/java/com/example/cs496_1stweek/contact/ContactFragment.kt
@@ -3,26 +3,49 @@ package com.example.cs496_1stweek.contact
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
-import android.database.Cursor
-import android.os.Build
 import android.os.Bundle
 import android.provider.ContactsContract
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cs496_1stweek.R
 
 class ContactFragment : Fragment() {
     @SuppressLint("Recycle")
-    @RequiresApi(Build.VERSION_CODES.O)
+    fun loadcontact() : MutableList<RecycleView> {
+        // 전화번호
+        val phoneUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+        //val phoneUri = ContactsContract.Contacts.CONTENT_URI
+        val projections = arrayOf(
+            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+            ContactsContract.CommonDataKinds.Phone.NUMBER,
+            ContactsContract.CommonDataKinds.Phone.PHOTO_THUMBNAIL_URI,
+        )
+        val contactItemList = mutableListOf<RecycleView>()
+        val cursor =
+            requireActivity().contentResolver.query(phoneUri, projections, null, null, null)
+
+        while(cursor?.moveToNext() == true) {
+            val name = cursor.getString(0)
+            val number = cursor.getString(1)
+            var pic = cursor.getString(2)
+            // 개별 전화번호 데이터 생성
+            if(pic==null) pic="null"
+            val phone = RecycleView(pic, name, number)//val phone = RecycleView(id, name, number)
+
+            // 결과목록에 더하기
+            contactItemList.add(phone)
+        }
+        return contactItemList
+    }
+
+
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -32,56 +55,28 @@ class ContactFragment : Fragment() {
         val recycleView: RecyclerView = contact_view.findViewById(R.id.contact_recycler_view)
 
         val contactItemList = mutableListOf<RecycleView>()
-        // 주소록
-        val addressUri = ContactsContract.Contacts.CONTENT_URI
-        // 전화번호
-        val phoneUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI
-        // 표시할 정보
-        val projections = arrayOf(
-            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-            ContactsContract.CommonDataKinds.Phone.NUMBER,
-            ContactsContract.CommonDataKinds.Phone.PHOTO_THUMBNAIL_URI,
-        )
+        val adapter = ContactAdapter(this, contactItemList)
 
         val requestPermissionLauncher =
             registerForActivityResult(
                 ActivityResultContracts.RequestPermission()
             ) { isGranted: Boolean ->
                 if (isGranted) {
-                    val cursor =
-                        requireActivity().contentResolver.query(addressUri, projections, null, null)
-                    while(cursor?.moveToNext() == true) {
-                        Log.d("hello","counting")
-                        val name = cursor.getString(0)
-                        val number = cursor.getString(1)
-                        var pic = cursor.getString(2)
-                        // 개별 전화번호 데이터 생성
-                        val phone = RecycleView(pic, name, number)
-
-                        // 결과목록에 더하기
-                        contactItemList.add(phone)
+                    val numbersIterator = loadcontact().iterator()
+                    while(numbersIterator.hasNext()) {
+                        contactItemList.add(numbersIterator.next())
                     }
+                    adapter.notifyDataSetChanged()
                 } else {
                     Toast.makeText(getActivity(),"앱을 사용하기 위해서 권한을 허용해야합니다.",Toast.LENGTH_SHORT).show();
-
                 }
             }
 
         when{
             ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_CONTACTS)== PackageManager.PERMISSION_GRANTED -> {
-                val cursor =
-                    requireActivity().contentResolver.query(phoneUri, projections, null, null, null)
-
-                while(cursor?.moveToNext() == true) {
-                    val name = cursor.getString(0)
-                    val number = cursor.getString(1)
-                    var pic = cursor.getString(2)
-                    // 개별 전화번호 데이터 생성
-                    if(pic==null) pic="null"
-                    val phone = RecycleView(pic!!, name, number)//val phone = RecycleView(id, name, number)
-
-                    // 결과목록에 더하기
-                    contactItemList.add(phone)
+                val numbersIterator = loadcontact().iterator()
+                while(numbersIterator.hasNext()) {
+                    contactItemList.add(numbersIterator.next())
                 }
             }
             else->{
@@ -90,22 +85,8 @@ class ContactFragment : Fragment() {
             }
         }
 
-        /*val cursor =  requireActivity().contentResolver.query(addressUri, projections, null, null )
-
-        while(cursor?.moveToNext() == true) {
-            val id = cursor.getString(0).toInt()
-            val name = cursor.getString(1)
-            val number = cursor.getString(2)
-            // 개별 전화번호 데이터 생성
-            val phone = RecycleView(id, name, number)
-
-            // 결과목록에 더하기
-            contactItemList.add(phone)
-        }*/
-        Log.d("hello",contactItemList.size.toString())
-
-        recycleView.adapter = ContactAdapter(this, contactItemList)
+        recycleView.adapter = adapter
         return contact_view
-}
+    }
 }
 
