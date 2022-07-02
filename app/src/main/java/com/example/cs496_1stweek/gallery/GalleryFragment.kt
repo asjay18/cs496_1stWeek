4baf533d03890cc1d64e7ee4a8dfc54dd117c55e
asjay18 <sejin1314@korea.ac.kr>
2022-07-02 14:50:44 +0900

    rotate, uploaddate 추가

--- app/src/main/java/com/example/cs496_1stweek/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/cs496_1stweek/gallery/GalleryFragment.kt
@@ -5,9 +5,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.LinearLayout
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -22,12 +19,16 @@ class GalleryFragment : Fragment() {
 
 
     fun loadImages() : MutableList<GalleryItem>{
-        val galleryItemList = mutableListOf<GalleryItem>()
+        var galleryItemList = mutableListOf<GalleryItem>()
         val imagesIterator = GalleryFileRead().main(requireContext())?.iterator()
         while(imagesIterator!!.hasNext()) {
-            val imageuri = "file:///data/user/0/com.example.cs496_1stweek/files/" + imagesIterator.next()
-            Log.d("check", imageuri.takeLast(10))
-            galleryItemList.add(GalleryItem(imageuri))
+            val checkCustomString = imagesIterator.next()
+            if(checkCustomString.length > 15) {
+                val imageuri = "file:///data/user/0/com.example.cs496_1stweek/files/" + checkCustomString
+                val date = imageuri.takeLast(10)
+                val generateDate = "20"+date[0]+date[1]+"."+date[2]+date[3]+"."+date[4]+date[5]+" "+date[6]+date[7]+":"+date[8]+date[9]
+                galleryItemList.add(0,GalleryItem(imageuri, generateDate))
+            }
         }
         return galleryItemList
     }
@@ -56,7 +57,9 @@ class GalleryFragment : Fragment() {
             if(it != null) {
                 val imageUri = it
                 val newuri = GalleryFileWrite().main(requireContext(), imageUri).toString()
-                galleryItemList.add(GalleryItem(newuri))
+                val date = newuri.takeLast(10)
+                val generateDate = "20"+date[0]+date[1]+"."+date[2]+date[3]+"."+date[4]+date[5]+" "+date[6]+date[7]+":"+date[8]+date[9]
+                galleryItemList.add(0, GalleryItem(newuri, generateDate))
                 adapter.notifyDataSetChanged()
             }
         }
@@ -70,7 +73,7 @@ class GalleryFragment : Fragment() {
 
 
         }
-
+        Log.d("checking", galleryItemList.size.toString())
         //loadImages()
         return galleryView
     }
