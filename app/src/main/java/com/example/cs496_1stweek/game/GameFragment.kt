19a860e718ecf4595f930a157b579d4e2b7237bf
asjay18 <sejin1314@korea.ac.kr>
2022-07-05 12:41:03 +0900

    gameretry popup 구현

--- app/src/main/java/com/example/cs496_1stweek/game/GameFragment.kt
+++ app/src/main/java/com/example/cs496_1stweek/game/GameFragment.kt
@@ -218,23 +218,23 @@ class GameFragment : Fragment(), DialogInterface {
         val answerButton3 : ImageButton = gameView.findViewById(R.id.answer3)
         val answerButton4 : ImageButton = gameView.findViewById(R.id.answer4)
 
-        val ans1_text : TextView = gameView.findViewById(R.id.ans1_text)
-        val ans2_text : TextView = gameView.findViewById(R.id.ans2_text)
-        val ans3_text : TextView = gameView.findViewById(R.id.ans3_text)
-        val ans4_text : TextView = gameView.findViewById(R.id.ans4_text)
+        val ans1Text : TextView = gameView.findViewById(R.id.ans1_text)
+        val ans2Text : TextView = gameView.findViewById(R.id.ans2_text)
+        val ans3Text : TextView = gameView.findViewById(R.id.ans3_text)
+        val ans4Text : TextView = gameView.findViewById(R.id.ans4_text)
 
         val enterButton : Button = gameView.findViewById(R.id.see_result_button)
 
-        writeButton1.setOnClickListener { writeOnButtons(1, answerButton1, answerButton2, answerButton3, answerButton4, ans1_text, ans2_text, ans3_text, ans4_text) }
-        writeButton2.setOnClickListener { writeOnButtons(2, answerButton1, answerButton2, answerButton3, answerButton4, ans1_text, ans2_text, ans3_text, ans4_text) }
-        writeButton3.setOnClickListener { writeOnButtons(3, answerButton1, answerButton2, answerButton3, answerButton4, ans1_text, ans2_text, ans3_text, ans4_text) }
-        writeButton4.setOnClickListener { writeOnButtons(4, answerButton1, answerButton2, answerButton3, answerButton4, ans1_text, ans2_text, ans3_text, ans4_text) }
-        writeButton5.setOnClickListener { writeOnButtons(5, answerButton1, answerButton2, answerButton3, answerButton4, ans1_text, ans2_text, ans3_text, ans4_text) }
-        writeButton6.setOnClickListener { writeOnButtons(6, answerButton1, answerButton2, answerButton3, answerButton4, ans1_text, ans2_text, ans3_text, ans4_text) }
-        writeButton7.setOnClickListener { writeOnButtons(7, answerButton1, answerButton2, answerButton3, answerButton4, ans1_text, ans2_text, ans3_text, ans4_text) }
-        writeButton8.setOnClickListener { writeOnButtons(8, answerButton1, answerButton2, answerButton3, answerButton4, ans1_text, ans2_text, ans3_text, ans4_text) }
-        writeButton9.setOnClickListener { writeOnButtons(9, answerButton1, answerButton2, answerButton3, answerButton4, ans1_text, ans2_text, ans3_text, ans4_text) }
-        writeButton0.setOnClickListener { writeOnButtons(0, answerButton1, answerButton2, answerButton3, answerButton4, ans1_text, ans2_text, ans3_text, ans4_text) }
+        writeButton1.setOnClickListener { writeOnButtons(1, answerButton1, answerButton2, answerButton3, answerButton4, ans1Text, ans2Text, ans3Text, ans4Text) }
+        writeButton2.setOnClickListener { writeOnButtons(2, answerButton1, answerButton2, answerButton3, answerButton4, ans1Text, ans2Text, ans3Text, ans4Text) }
+        writeButton3.setOnClickListener { writeOnButtons(3, answerButton1, answerButton2, answerButton3, answerButton4, ans1Text, ans2Text, ans3Text, ans4Text) }
+        writeButton4.setOnClickListener { writeOnButtons(4, answerButton1, answerButton2, answerButton3, answerButton4, ans1Text, ans2Text, ans3Text, ans4Text) }
+        writeButton5.setOnClickListener { writeOnButtons(5, answerButton1, answerButton2, answerButton3, answerButton4, ans1Text, ans2Text, ans3Text, ans4Text) }
+        writeButton6.setOnClickListener { writeOnButtons(6, answerButton1, answerButton2, answerButton3, answerButton4, ans1Text, ans2Text, ans3Text, ans4Text) }
+        writeButton7.setOnClickListener { writeOnButtons(7, answerButton1, answerButton2, answerButton3, answerButton4, ans1Text, ans2Text, ans3Text, ans4Text) }
+        writeButton8.setOnClickListener { writeOnButtons(8, answerButton1, answerButton2, answerButton3, answerButton4, ans1Text, ans2Text, ans3Text, ans4Text) }
+        writeButton9.setOnClickListener { writeOnButtons(9, answerButton1, answerButton2, answerButton3, answerButton4, ans1Text, ans2Text, ans3Text, ans4Text) }
+        writeButton0.setOnClickListener { writeOnButtons(0, answerButton1, answerButton2, answerButton3, answerButton4, ans1Text, ans2Text, ans3Text, ans4Text) }
 
         answerButton1.setOnClickListener { changePos(1, answerButton1, answerButton2, answerButton3, answerButton4) }
         answerButton2.setOnClickListener { changePos(2, answerButton1, answerButton2, answerButton3, answerButton4) }
@@ -242,27 +242,25 @@ class GameFragment : Fragment(), DialogInterface {
         answerButton4.setOnClickListener { changePos(4, answerButton1, answerButton2, answerButton3, answerButton4) }
 
         enterButton.setOnClickListener {
-            answerButton1.setImageResource(R.drawable.input_text)
-            answerButton2.setImageResource(R.drawable.button_default)
-            answerButton3.setImageResource(R.drawable.button_default)
-            answerButton4.setImageResource(R.drawable.button_default)
-            if(canPlay) getResult(numberA, ans1_text, ans2_text, ans3_text, ans4_text)
+            if(ans1Text.text==" "||ans2Text.text==" "||ans3Text.text==" "||ans4Text.text==" "){
+                Toast.makeText(requireContext(),"4자리 숫자를 입력해주세요", Toast.LENGTH_SHORT).show()
+            }
             else{
-                ans1_text.text=" "
-                ans2_text.text=" "
-                ans3_text.text=" "
-                ans4_text.text=" "
-                if(lose){
-                    val deadDialog : GameDeadPopup = GameDeadPopup(this, ("0"+numberA.toString()).takeLast(4)).getInstance(this, ("0"+numberA.toString()).takeLast(4))
-                    activity?.supportFragmentManager?.let{
-                            fragmentManager -> deadDialog.show(fragmentManager, "You're Dead")
-                    }
-                }
+                answerButton1.setImageResource(R.drawable.input_text)
+                answerButton2.setImageResource(R.drawable.button_default)
+                answerButton3.setImageResource(R.drawable.button_default)
+                answerButton4.setImageResource(R.drawable.button_default)
+                if(canPlay) getResult(numberA, ans1Text, ans2Text, ans3Text, ans4Text)
                 else{
-                    val winDialog : GameWinPopup = GameWinPopup(this, ("0"+numberA.toString()).takeLast(4)).getInstance(this, ("0"+numberA.toString()).takeLast(4))
+                    ans1Text.text=" "
+                    ans2Text.text=" "
+                    ans3Text.text=" "
+                    ans4Text.text=" "
+                    val retryDialog : GameRetryPopup = GameRetryPopup(this, lose).getInstance(this, lose)
                     activity?.supportFragmentManager?.let{
-                            fragmentManager -> winDialog.show(fragmentManager, "You're Dead")
+                            fragmentManager -> retryDialog.show(fragmentManager, "Try again")
                     }
+
                 }
             }
         }
